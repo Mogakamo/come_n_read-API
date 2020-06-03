a2a7669fcbb81aea5809ef5a0f6847f62e008e2b
Mogakamo <mogaka.amo254@gmail.com>
2020-06-03 21:35:30 +0300

    Initial Commit

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+module.export = {
+    BookSales: require("./booksales"),
+    Stores: require("./stores"),
+    Employees: require("./employees"),
+    ClientReviews: require("./clientreviews"),
+    Clients: require("./clients"),
+    Books: require("./books"),
+    Authors: require("./authors")
+}
+
+var controllers = require("/controllers")
+
